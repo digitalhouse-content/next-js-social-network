5a6c11b8a2d129e4f31c7dfaf77ff34eb34aa0eb
Tomas de Priede <tomasdepriede@gmail.com>
2023-08-10 00:08:59 -0300

    S3-M1-V7 Instalacion Storybook completado.

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,5 +1,64 @@
+import Message from "@/components/messages/Message";
+import Link from "next/link";
+
 const UserPage = ({params}: {params: {username: string}}) => {
-    return <div>Nombre de usuario: {params.username}</div>
+    
+    const user = {
+        username: params.username,
+        name: 'Anakin Skywalker',
+        bio: 'Vengo de Tatooine',
+        followersCount: 15,
+        followingCount: 3,
+        messages: [
+            {
+                name: 'Anakin Skywalker',
+                username: 'anakin',
+                message: 'Segundo mensaje',
+                repliesCount: 13
+            }
+            ,{
+                name: 'Anakin Skywalker',
+                username: 'anakin',
+            message: 'Primer mensaje',
+            repliesCount: 13
+        }],
+        replies: [
+            {
+                message: 'Mi respuesta',
+                repliesCount: 0
+            }
+        ]
+    }
+    
+    return <main className="flex flex-col bg-gray-100 p-8">
+        <section className="flex flex-col mb-8">
+            <div className="rounded-full p-6 bg-gray-300 w-20 text-center mb-4">
+                <span className="font-semibold text-lg">AS</span>
+            </div>
+            <h2 className="mb-1">
+                {user.name}
+            </h2>
+            <div className="text-md mb-4 text-gray-600 cursor-pointer">
+                @<Link href={`/users/${user.username}`}>{user.username}</Link>
+            </div>
+            <div className="mb-4">
+                {user.bio}
+            </div>
+            <div className="flex justify-between mb-4">
+                <div><span className="font-semibold">{user.followersCount}</span> Seguidores</div>
+                <div><span className="font-semibold">{user.followingCount}</span> Siguiendo</div>
+            </div>
+        </section>
+        <div className="flex justify-evenly mb-4">
+            <div className="cursor-pointer border-b-4 border-blue-400">Mensajes</div>
+            <div className="cursor-pointer">Respuestas</div>
+        </div>
+        <div>
+            {user.messages.map((message, index) => 
+                <Message key={`${index}`} message={message}/>)
+            }
+        </div>
+    </main>
 }
 
 export default UserPage;
