d5af7914fcbcbdfc9b92768eb18674da166dc06a
Tomas de Priede <tomasdepriede@gmail.com>
2023-09-03 19:14:23 -0300

    S13-M4-V36 Instalación de Redis. completado

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,18 +2,31 @@ import authApi from '@/services/auth/auth.service';
 import * as yup from "yup";
 import { NextResponse, type NextRequest } from 'next/server'
 import { AccessDeniedError } from '@/services/common/http.errors';
+import { createClient } from 'redis';
+import { v4 as uuidv4 } from 'uuid';
 
 const schema = yup.object({
     username: yup.string().required(),
     password: yup.string().required()
 }).required();
 
+const client = createClient({
+    url: 'redis://default:SocialNetworkPass@localhost:6379'
+});
+
+client.connect().then(() => {
+    console.log('connected to redis')
+})
+
+const ONE_MINUTE = 60;
+
 export async function POST(request: Request) {
     const {username, password} = await schema.validate(await request.json());
     try{
         const loginResponse = await authApi.login(username, password);
+        const sessionId = uuidv4();
+        client.set(sessionId, loginResponse.accessToken, {EX: ONE_MINUTE})
 
-        const sessionId = loginResponse.accessToken;
         return NextResponse.json({sessionId, username});
     }catch (e){
         if (e instanceof AccessDeniedError){
